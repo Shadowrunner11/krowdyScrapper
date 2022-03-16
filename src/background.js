59bc05724213636aee34f86228c034f158342dd4
Shadow <matematicadivina1@gmail.com>
2022-03-16 18:13:21 -0500

    second commit

--- src/background.js
+++ src/background.js
@@ -12,9 +12,11 @@ chrome.runtime.onConnect.addListener(port=>{
      if (port.name==="safePort"){
           port.onMessage.addListener(mensaje=>{
                console.log(mensaje)
+               if (!db.isOpen()) db.open();
                db.products.add(mensaje)
-                    .then(response=>{console.log(response)})
+                    .then(response=>{console.log(response, "aniadido con exito"); console.log(db.products.toArray())})
                     .catch(error => console.log(error)) 
+               db.close()
           })
              
      }
